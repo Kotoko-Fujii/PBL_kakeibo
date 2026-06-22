61083ec49e2d0f6fe76cc6c2f2879dc5301e55ae
24ms1063kubo <manamana12080205@gmail.com>
2026-06-22 16:55:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ def handle_message(event):
                     budget = get_budget(ws)
                     today_spent = get_today_spent(ws, today_str)
                     
-                    # ユーザー専用のタブに追加される
+                
                    # ユーザー専用のタブに追加される（A〜D列を基準にするように修正）
                     ws.append_row([date_str, item_name, item_price, category], table_range="A:D")
                     
