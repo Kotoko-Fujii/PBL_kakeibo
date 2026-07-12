9b6d7bb9c82436556abaeac8d6141143581f58f3
24ms1026 <24ms1026@meijigakuin.ac.jp>
2026-07-13 02:03:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -388,8 +388,8 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(
     send_morning_reminder, 
     'cron', 
-    hour=8, 
-    minute=0, 
+    hour=2, 
+    minute=6, 
     timezone=pytz.timezone('Asia/Tokyo')
 )
 scheduler.start()
